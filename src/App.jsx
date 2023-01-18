814ffcc6410559aeb34858bccee62a1522421248
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-01-18 18:29:03 +0530

    layouting the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import s from "./style.module.css";
 export function App() {
-  return <div>Hello! Welcome to React Movie Database!!</div>;
+  return (
+    <div className={s.main_container}>
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <div>LOGO</div>
+            <div>Subtitle</div>
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <input style={{ width: " 100%" }} type="text" />
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_detail}>TV Show details</div>
+      <div className={s.recommended_tv_shows}>Recommended TV Shows..</div>
+    </div>
+  );
 }
